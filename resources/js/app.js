a315eb97c9516569de46c207ae555c51d3434d92
Piccolo <kabiryusufbashir@gmail.com>
2021-11-01 12:39:38 +0100

    Created NavComponent

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue';
 
 let app = createApp({})
 
-app.component('cart', require('./components/CartComponent.vue').default);
+app.component('nav-component', require('./components/NavComponent.vue').default);
+app.component('cart-component', require('./components/CartComponent.vue').default);
 app.component('product', require('./components/ProductComponent.vue').default);
 
 app.mount(".app")
