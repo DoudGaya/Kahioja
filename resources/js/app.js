efd5c831db15ebd6a2e5f0ebb5d200188156f547
Piccolo <kabiryusufbashir@gmail.com>
2021-11-16 20:54:35 +0100

    Finally get the csrf working from vuejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import { store } from './store/index'
+import Flutterwave from  'flutterwave-vue-v3'
 
 window.axios = require('axios')
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+// window.axios.defaults.headers.common['X-CSRF-TOKEN'] = window.Laravel.csrfToken;
 
 let app = createApp({})
 app.component('nav-component', require('./components/NavComponent.vue').default)
@@ -11,7 +14,8 @@ app.component('product-description-component', require('./components/productDesc
 app.component('category-component', require('./components/categoryComponent.vue').default)
 app.component('load-component', require('./components/loadMoreComponent.vue').default)
 app.component('checkout-component', require('./components/CheckOutComponent.vue').default)
-app.use(store).mount('#app')
+app.use(store).use(Flutterwave, { publicKey: 'FLWPUBK-73e9051112aa802dbf62f33100f25bfd-X' } )
+.mount('#app')
 
 // let product = createApp({})
 // product.mount('#product')
--- resources/views/front/checkout.blade.php
+++ resources/views/front/checkout.blade.php
@@ -3,6 +3,8 @@
 @section('main')
     <!-- Banner -->
     <div id="banner" class="relative top-36 md:top-0 mt-6 md:px-14 px-4">
-        <checkout-component></checkout-component>
+        <form action="{{ route('flutterwave.submit') }}" method="POST">
+            @csrf
+            <checkout-component></checkout-component>
     </div>
 @endsection
