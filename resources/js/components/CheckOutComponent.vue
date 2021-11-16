45e688c6c69062857b75809e0752b513280d3c51
Piccolo <kabiryusufbashir@gmail.com>
2021-11-16 12:23:58 +0100

    Create checkout delivery details section completed

--- resources/js/components/CheckOutComponent.vue
+++ resources/js/components/CheckOutComponent.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="grid grid-cols-2 gap-12">
             <div>
-                <div class="py-4 px-4 bg-gray-900 text-white text-2xl rounded-lg">
+                <div class="py-4 px-4 bg-gray-900 text-white text-2xl rounded-lg font-extrabold">
                     Delivery Information
                 </div>
                 <div class="my-6 py-4 px-16 rounded-lg yus-shadow">
@@ -33,7 +33,90 @@
                     </div>
                 </div>
             </div>
-            <div>2</div>
+            <div>
+                <div class="py-4 px-4 bg-gray-300 text-black text-2xl rounded-lg font-extrabold">
+                    Your Order
+                </div>
+                <div id="checkout-cart-body" class="my-6 py-4 px-16 rounded-lg yus-shadow">
+                    <div v-if="cartNo > 0" class="my-5">
+                        <div :key="product.product_id" v-for="product in getAllCart" id="cart-body-products" class="p-4">
+                            <div class="grid grid-cols-3 gap-6 items-center">
+                                <div id="cart-body-product-image">
+                                    <img :src="`/images/products/${product.photo}`" :alt="`${ product.name }`">
+                                </div>
+                                <div>
+                                    <div id="cart-body-product-title">{{ product.name }} x {{product.quantity}}</div>
+                                    <div id="cart-body-product-delivery-time" class="flex my-4">
+                                        <span>
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.93408 6.64286H12.5357" stroke="#222222" stroke-width="1.35205" stroke-miterlimit="10" stroke-linecap="round"/><path d="M5.93408 9.31318H9.79123" stroke="#222222" stroke-width="1.35205" stroke-miterlimit="10" stroke-linecap="round"/><path d="M8.70742 20.25H3.71741C3.52714 20.25 3.34467 20.1744 3.21013 20.0399C3.07558 19.9053 3 19.7229 3 19.5326V4.46741C3 4.27714 3.07558 4.09467 3.21013 3.96013C3.34467 3.82558 3.52714 3.75 3.71741 3.75H20.2826C20.4729 3.75 20.6553 3.82558 20.7899 3.96013C20.9244 4.09467 21 4.27714 21 4.46741V12.6758" stroke="#222222" stroke-width="1.35205" stroke-miterlimit="10" stroke-linecap="round"/><path d="M12.8282 21.3916C13.4029 21.3916 13.8688 20.9257 13.8688 20.351C13.8688 19.7762 13.4029 19.3104 12.8282 19.3104C12.2535 19.3104 11.7876 19.7762 11.7876 20.351C11.7876 20.9257 12.2535 21.3916 12.8282 21.3916Z" stroke="#222222" stroke-width="1.35205" stroke-miterlimit="10" stroke-linecap="round"/><path d="M19.9689 21.3916C20.5436 21.3916 21.0095 20.9257 21.0095 20.351C21.0095 19.7762 20.5436 19.3104 19.9689 19.3104C19.3942 19.3104 18.9283 19.7762 18.9283 20.351C18.9283 20.9257 19.3942 21.3916 19.9689 21.3916Z" stroke="#222222" stroke-width="1.35205" stroke-miterlimit="10" stroke-linecap="round"/><path d="M11.788 20.3796H11.1921C11.1716 20.3796 11.152 20.3714 11.1375 20.357C11.123 20.3425 11.1148 20.3228 11.1148 20.3023V14.8242C11.1148 14.8037 11.123 14.7841 11.1375 14.7696C11.152 14.7551 11.1716 14.7469 11.1921 14.7469H19.4484C19.5066 14.7469 19.564 14.7601 19.6164 14.7854C19.6688 14.8107 19.7148 14.8475 19.7509 14.8931L21.7853 17.4579C21.8395 17.5261 21.869 17.6107 21.869 17.6979V20.3024C21.869 20.3125 21.867 20.3226 21.8631 20.332C21.8592 20.3413 21.8535 20.3498 21.8463 20.357C21.8392 20.3642 21.8306 20.3699 21.8213 20.3738C21.8119 20.3776 21.8018 20.3796 21.7917 20.3796H21.0097" stroke="#222222" stroke-width="1.35205" stroke-miterlimit="10" stroke-linecap="round"/><path d="M18.9286 20.3796H13.8688" stroke="#222222" stroke-width="1.35205" stroke-miterlimit="10" stroke-linecap="round"/><path d="M17.4836 14.747V20.3509" stroke="#222222" stroke-width="1.35205" stroke-miterlimit="10" stroke-linecap="round"/></svg>                 
+                                        </span>
+                                        <span class="relative -top-1 left-1">1 day within Kano</span>
+                                    </div>
+                                    <div class="hidden md:block">
+                                        <div class="cursor-pointer" @click="removeProduct(product.bagId)" id="cart-body-product-remove">
+                                            Remove Product
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                                <div>
+                                    <div id="cart-body-product-price">
+                                        N{{ product.subTotal }}
+                                    </div>
+                                    <div class="hidden md:block">
+                                        <div id="cart-body-product-add" class="flex justify-between w-3/4 py-2 px-4 rounded-full mt-11">
+                                            <div class="cursor-pointer">
+                                            <button @click="minusProduct(product.bagId, product.quantity)"> - </button>
+                                            </div>
+                                            <input class="w-1/3 text-center bg-white" :value="product.quantity" disabled>
+                                            <div class="cursor-pointer">
+                                                <button @click="addProduct(product.bagId, product.quantity)">+</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- //Mobile View  -->
+                            <div class="grid grid-cols-2 gap-6 md:hidden">
+                                <div class="cursor-pointer mx-auto" @click="removeProduct(product.bagId)" id="cart-body-product-remove">
+                                    Remove Product
+                                </div>
+                                <div id="cart-body-product-add" class=" flex justify-between w-3/4 py-2 px-4 rounded-full my-auto">
+                                    <div class="cursor-pointer">
+                                    <button @click="minusProduct(product.bagId, product.quantity)"> - </button>
+                                    </div>
+                                    <input class="w-1/3 text-center bg-white" :value="product.quantity" disabled>
+                                    <div class="cursor-pointer">
+                                        <button @click="addProduct(product.bagId, product.quantity)">+</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>           
+                </div>
+                <div class="py-4 px-4 bg-gray-300 text-black text-2xl rounded-lg font-extrabold">
+                    Order Summary
+                </div>
+                <div class="my-6 py-4 px-16 rounded-lg yus-shadow">
+                    <!-- Cart Total  -->
+                    <div v-if="cartNo > 0" class="py-5 my-5">
+                        <div class="grid grid-cols-2 gap-6 py-1 w-full font-bold">
+                            <div>Subtotal <span class="font-normal">(all products)</span></div>
+                            <div class="text-right">N{{ subTotal }}</div>
+                            <div>Delivery Fee</div>
+                            <div class="text-right">N{{ deliveryFee }}</div>
+                            <div>Estimated Total</div>
+                            <div class="text-right">N{{ estimatedTotal }}</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="my-6 py-4 px-16 rounded-lg yus-shadow">
+                    <div class="my-4 text-2xl font-extrabold">Delivery time</div>
+                    <p class="my-4">
+                        Orders are delivered on all days (Monday - Sunday) excluding public holidays
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -46,6 +129,11 @@ export default {
             displayCart: true,
             isLoading: false,
             authUser: window.authUser,
+            checkOutName: window.authUser.name,
+            checkOutAddress: window.authUser.address,
+            checkOutCity: window.authUser.city,
+            checkOutState: window.authUser.state,
+            checkOutPhone: window.authUser.phone
         }
     },
     computed: {
