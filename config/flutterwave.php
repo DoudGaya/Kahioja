5fa0a4a7e11328e59278545d05483754045460cc
Piccolo <kabiryusufbashir@gmail.com>
2021-12-16 20:37:47 +0100

    Pushing version 1.0.3

--- config/flutterwave.php
+++ config/flutterwave.php
@@ -15,17 +15,17 @@ return [
      * Public Key: Your Rave publicKey. Sign up on https://dashboard.flutterwave.com/ to get one from your settings page
      *
      */
-    'publicKey' => 'FLWPUBK-73e9051112aa802dbf62f33100f25bfd-X',
+    'publicKey' => 'FLWPUBK-48d2c74dd0398de75148e05e959722d2-X',
 
     /**
      * Secret Key: Your Rave secretKey. Sign up on https://dashboard.flutterwave.com/ to get one from your settings page
      *
      */
-    'secretKey' => 'FLWSECK-624a205752db641007231cb058e52fdd-X',
+    'secretKey' => 'FLWSECK-447e080585eaedb5b6764294a63cb0ed-X',
 
     /**
      * Prefix: Secret hash for webhook
      *
      */
-    'secretHash' => 'piccolo',
+    'secretHash' => 'kahioja',
 ];
