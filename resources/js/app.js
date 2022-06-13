94ce94ae3aef547f9bc981edabc1fccf5ad7d684
Piccolo <kabiryusufbashir@gmail.com>
2022-06-13 16:51:11 +0100

    Create Footer Component

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 
 let app = createApp({})
 app.component('nav-component', require('./components/NavComponent.vue').default)
+app.component('footer-component', require('./components/FooterComponent.vue').default)
 app.component('product-component', require('./components/productComponent.vue').default)
 app.component('product-details-component', require('./components/productDetailsComponent.vue').default)
 app.component('product-description-component', require('./components/productDescriptionComponent.vue').default)
